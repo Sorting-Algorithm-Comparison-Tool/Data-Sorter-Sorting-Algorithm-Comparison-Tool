0ccb8a95f656cae448100c3eb87b8dd6492a3d33
22UG3-0651 <22ug3-0651@sltc.ac.lk>
2025-11-05 11:58:27 +0530

    SortingComparisonTool

--- Sorting_Algorith.java
+++ Sorting_Algorith.java
@@ -1,3 +1,9 @@
 import java.util.*;
 
 public class SortingComparisonTool {
+
+     // BUBBLE SORT 
+        public static int bubbleSort(int[] arr) {
+        int n // Get how many numbers we have
+        int steps = 0; // Count how many comparisons we make
+        
